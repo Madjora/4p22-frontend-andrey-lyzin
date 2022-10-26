4fc949dd5fffba6d8e1d6e778fd9dee1040359f3
Andrey Lyzin <andreylyzin@yandex.ru>
2022-10-26 21:45:15 +0300

    add test introduction_js

---
+++ Lesson_js/introduction_js/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const circle = document.querySelector('.circle');
+
+document.addEventListener('mousemove', (event) => {
+    circle.style.left = '${event.clientX}px';
+    circle.style.top = '${event.clientX}px';
+});
